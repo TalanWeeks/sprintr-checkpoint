ef7e1851ddd9be68580085a010b050ce8765f1be
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-27 16:31:19 -0600

    c

--- sprintr-checkpoint/server/db/DbContext.js
+++ sprintr-checkpoint/server/db/DbContext.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { BacklogItemSchema } from '../models/BacklogItem.js'
 import { NoteSchema } from '../models/Note'
+import { ProjectSchema } from '../models/Project.js'
 import { SprintSchema } from '../models/Sprint'
 import { TaskSchema } from '../models/Task'
 import { ValueSchema } from '../models/Value'
@@ -15,6 +17,10 @@ class DbContext {
   Task = mongoose.model('Task', TaskSchema);
 
   Sprint = mongoose.model('Sprint', SprintSchema);
+
+  BacklogItem = mongoose.model('BacklogItem', BacklogItemSchema);
+
+  Project = mongoose.model('Project', ProjectSchema);
 }
 
 export const dbContext = new DbContext()
