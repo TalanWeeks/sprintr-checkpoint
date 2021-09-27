3de510de1db78ecb480b10ee9591eda62934a391
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-09-27 16:09:48 -0600

    dbcontenxt half way

--- sprintr-checkpoint/server/db/DbContext.js
+++ sprintr-checkpoint/server/db/DbContext.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { NoteSchema } from '../models/Note'
+import { SprintSchema } from '../models/Sprint'
+import { TaskSchema } from '../models/Task'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+
+  Note = mongoose.model('Note', NoteSchema);
+
+  Task = mongoose.model('Task', TaskSchema);
+
+  Sprint = mongoose.model('Sprint', SprintSchema);
 }
 
 export const dbContext = new DbContext()
