a3c1eb32283199c8bc723b72e66c58ec4970025a
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-28 16:31:02 -0600

    c

--- sprintr-checkpoint.client/src/pages/HomePage.vue
+++ sprintr-checkpoint.client/src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <div class="home-card p-5 bg-white rounded elevation-3">
       <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        This is a Test
+        This is a Test trololol
       </h1>
     </div>
   </div>
