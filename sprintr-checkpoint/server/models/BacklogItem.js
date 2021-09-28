8cb517ae74b92c346bb8a972942d7be7485aa736
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-28 15:02:33 -0600

    backlog passes tests

--- sprintr-checkpoint/server/models/BacklogItem.js
+++ sprintr-checkpoint/server/models/BacklogItem.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const BacklogItemSchema = new Schema({
-  name: { type: String },
-  description: { type: String },
-  status: { type: String, enum: ['pending', 'in-progress', 'review', 'done'] },
-  projectId: { type: Schema.Types.ObjectId },
-  sprintId: { type: Schema.Types.ObjectId },
-  creatorId: { type: Schema.Types.ObjectId },
+  name: { type: String, require: true },
+  description: { type: String, require: true },
+  status: { type: String, enum: ['pending', 'in-progress', 'review', 'done'], require: true },
+  projectId: { type: Schema.Types.ObjectId, require: true },
+  sprintId: { type: Schema.Types.ObjectId, require: true },
+  creatorId: { type: Schema.Types.ObjectId, require: true },
   color: { type: String }
 },
 { timestamps: true, toJSON: { virtuals: true } }
