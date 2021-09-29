141d22eee390823939313a0b908b224d5458b7e5
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-09-29 15:16:08 -0600

    project page styled

--- sprintr-checkpoint.client/src/components/Project.vue
+++ sprintr-checkpoint.client/src/components/Project.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Project">
     <div class="container-fluid">
-      <div class="row rounded my-2 py-3 bg-dark text-light">
+      <div class="row shadow rounded my-2 py-3 bg-dark text-light">
         <div class="col-4 selectable">
           <router-link :to="{name: 'Project', params: {id: project.id}}" class="selectable">
             <h4>
