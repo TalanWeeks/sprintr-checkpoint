d4434409bc9751c58b8ea21d134deeb7882f45f9
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-29 15:25:17 -0600

    backlogItems page, service, model, comp

---
+++ sprintr-checkpoint.client/src/pages/BacklogPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="backlogItems">
+    <div class="container">
+      <BacklogItem v-for="b in backlogItems" :key="b.id" :backlog-item="b" />
+    </div>
+  </div>
+  <div v-else>
+    ...loading....
+  </div>
+</template>
+
+<script>
+import { computed, onMounted } from '@vue/runtime-core'
+import { AppState } from '../AppState.js'
+import Pop from '../utils/Pop.js'
+import { backlogItemsService } from '../services/BacklogItemsService.js'
+export default {
+  setup() {
+    const currentProject = computed(() => AppState.currentProject)
+    onMounted(async() => {
+      try {
+        await backlogItemsService.getBacklogItems(currentProject.id)
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
+    return {
+      getBacklogItems: computed(() => AppState.backlogItems)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ sprintr-checkpoint.client/src/services/BacklogItemsService.js
@@ -0,0 +1,21 @@
+import { AppState } from '../AppState.js'
+import { BacklogItem } from '../models/BacklogItem.js'
+import { logger } from '../utils/Logger.js'
+import { api } from './AxiosService.js'
+
+class BacklogItemsService {
+  async getBacklogItems(projectId) {
+    AppState.backlogItems = []
+    const res = await api.get(`api/projects/${projectId}/backlog`)
+    logger.log('getBacklogItems', res)
+    AppState.backlogItems = res.data.map(b => new BacklogItem(b))
+  }
+
+  async createBacklogItem(newItem, projectId) {
+    const res = await api.post(`api/projects/${projectId}/backlog`)
+    logger.log('createBacklogItem', res)
+    AppState.backlogItems.shift(new BacklogItem(res.data))
+  }
+}
+
+export const backlogItemsService = new BacklogItemsService()
