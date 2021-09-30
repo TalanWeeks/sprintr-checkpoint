5f7624a800bbf771563a329c49adfdd64ab3244d
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-30 13:30:56 -0600

    tasks started

---
+++ sprintr-checkpoint.client/src/services/TaskService.js
@@ -0,0 +1,21 @@
+import { AppState } from '../AppState.js'
+import { Task } from '../models/Task.js'
+import { logger } from '../utils/Logger.js'
+import { api } from './AxiosService.js'
+
+class TasksService {
+  async getTasks(projectId, backlogItemId) {
+    AppState.tasks = []
+    const res = await api.get(`api/projects/${projectId}/tasks`)
+    const filtered = res.data.filter(t => t.backlogItemId === backlogItemId)
+    AppState.tasks = filtered.map(t => new Task(t))
+  }
+
+  async createTask(newTask, projectId) {
+    const res = await api.post(`api/projects/${projectId}/tasks`)
+    logger.log('createTask', res)
+    AppState.tasks = [...AppState.tasks, (new Task(res.data))]
+  }
+}
+
+export const tasksService = new TasksService()
