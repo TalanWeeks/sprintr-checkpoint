74555a06daa5c77af09ea2b56ea1f3edc7e93dc2
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-09-30 12:49:13 -0600

    pushup

--- sprintr-checkpoint.client/src/components/BacklogItem.vue
+++ sprintr-checkpoint.client/src/components/BacklogItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Backlog">
     <div class="container-fluid">
-      <div class="row shadow rounded my-2 py-3 bg-dark text-light">
+      <div class="row shadow rounded mb-4 py-3 bg-dark text-light">
         <div class="col-4">
           <h4>
             Backlog Title:
@@ -14,14 +14,35 @@
           <span class="text-success">{{ backlogItem.status }}</span>
         </div>
         <div class="col-4 text-center">
-          <button class="btn btn-success m-2" :data-bs-target="'#note-form-' + backlogItem.id" data-bs-toggle="modal">
-            Create Note
-          </button>
-          <button class="btn btn-success m-2" :data-bs-target="'#notes-' + backlogItem.id" data-bs-toggle="modal">
-            Show Note's
-          </button>
           <div class=" m-0 p-2">
             <i class="mdi mdi-delete-forever text-danger f-20 selectable m-0" @click="deleteBacklogItem(backlogItem.id)" v-if="account.id == backlogItem.creatorId"></i>
+          </div>
+          <div class="dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Backlog Options
+            </button>
+            <ul class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <button class="btn btn-success m-2" :data-bs-target="'#note-form-' + backlogItem.id" data-bs-toggle="modal">
+                  Create Note
+                </button>
+              </li>
+              <li>
+                <button class="btn btn-success m-2" :data-bs-target="'#notes-' + backlogItem.id" data-bs-toggle="modal">
+                  Show Notes
+                </button>
+              </li>
+              <li>
+                <button class="btn btn-success m-2" :data-bs-target="'#tasks-form-' + backlogItem.id" data-bs-toggle="modal">
+                  Create Tasks
+                </button>
+              </li>
+              <li>
+                <button class="btn btn-success m-2" :data-bs-target="'#tasks-' + backlogItem.id" data-bs-toggle="modal">
+                  Show Tasks
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -57,7 +78,7 @@ export default {
   setup(props) {
     return {
       backlogItems: () => AppState.backlogItems,
-
+      account: () => AppState.account,
       async deleteBacklogItem(BacklogItemId) {
         try {
           await backlogItemsService.deleteBacklogItem(BacklogItemId)
